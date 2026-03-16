e6b66281c67a42f3c259c4b67b89f3b4899e921d
Not-MilanGurung <notmilangurung@gmail.com>
2026-03-16 09:39:45 +0545

    Simple NavBar implementation Needs further work later on

--- recipe-nest-frontend/src/components/NavBar.jsx
+++ recipe-nest-frontend/src/components/NavBar.jsx
@@ -1,17 +1,25 @@
-import {Link} from 'react-router';
+import {NavLink} from 'react-router';
 
 function NavBar() {
-  return (
-	<nav className='flex justify-around bg-blue-400'>
-		<div>RecipeNest</div>
-		<div>
-			<Link to="/">Home</Link>
-			<Link to="/recipes">Recipes</Link>
-			<Link to="/chefs">Chefs</Link>
-			<Link to="/contact">Contact</Link>
-		</div>
-	</nav>
-  )
+	const navLinkStyle = ({ isActive}) => `
+		h-full px-4 flex items-center  cursor-pointer
+		${isActive ? `bg-neutral` : `hover:bg-linear-to-b from-50% to-secondary`}
+	`;
+	return (
+		<header className='flex h-20 justify-between pl-[10%] pr-[5%] items-center bg-primary text-secondary'>
+			<div className='text-4xl font-bold'>RecipeNest</div>
+			<nav className='h-full flex text-xl'>
+				<NavLink className={navLinkStyle} to="/">Home</NavLink>
+				<NavLink className={navLinkStyle} to="/recipes">Recipes</NavLink>
+				<NavLink className={navLinkStyle} to="/chefs">Chefs</NavLink>
+				<NavLink className={navLinkStyle} to="/contact">Contact</NavLink>
+			</nav>
+			<div className='space-x-4 text-xl'>
+				<NavLink to="/login" className='bg-neutral rounded-xl px-4 py-1 cursor-pointer hover:bg-neutral/75'>Login</NavLink>
+				<NavLink to="/register" className='bg-neutral rounded-xl px-4 py-1 cursor-pointer hover:bg-neutral/75'>Register</NavLink>
+			</div>
+		</header>
+	)
 }
 
 export default NavBar
--- recipe-nest-frontend/src/routes/AppRoute.jsx
+++ recipe-nest-frontend/src/routes/AppRoute.jsx
@@ -4,6 +4,8 @@ import HomePage from "../pages/HomePage";
 import RecipesPage from "../pages/RecipesPage";
 import ChefsPage from "../pages/ChefsPage";
 import ContactPage from "../pages/ContactPage";
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
 
 function AppRoute() {
   return (
@@ -12,6 +14,9 @@ function AppRoute() {
 		<Route path="/recipes" element={<RecipesPage />} />
 		<Route path="/chefs" element={<ChefsPage />} />
 		<Route path="/contact" element={<ContactPage />} />
+		<Route path="/login" element={<LoginPage />} />
+		<Route path="/register" element={<RegisterPage />} />
+
 	</Routes>
   )
 }
